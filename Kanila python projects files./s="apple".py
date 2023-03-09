449fca965de6f1788f417ff84846afd22ef6e376
Kanila132004 <vijayakumarkanila@gmail.com>
2023-03-09 23:17:06 +0530

    positive or negative

--- Kanila python projects files./s="apple".py
+++ Kanila python projects files./s="apple".py
@@ -1,3 +1,10 @@
 s="apple"
 print(s[::-1])
 print(s[1:5])
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
